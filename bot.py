1e5f8db366e6ec7e77807fd1c7e73217efefac01
Youssef <you.zouhairi@gmail.com>
2023-10-19 17:16:43 -0400

    Adding the slack event, and initial handling.

--- bot.py
+++ bot.py
@@ -1,9 +1,33 @@
 import slack
 import os
+import json
 from pathlib import Path
 from dotenv import load_dotenv
+from flask import Flask
+from slackeventsapi import SlackEventAdapter
 
 env_path = Path('.')/'.env'
 load_dotenv(dotenv_path=env_path)
 
+app = Flask(__name__)
+slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/events',app)
+
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+BOT_ID = client.api_call("auth.test")['user_id']
+#print(client.api_call("auth.test"))
+
+@slack_event_adapter.on('message')
+def message(payload):
+    event = payload.get('event',{})
+    channel_id = event.get('channel')
+    user_id = event.get('user')
+    text = event.get('text')
+    if BOT_ID != user_id:
+        client.chat_postMessage(channel=channel_id, text=text)
+
+
+
+#client.chat_postMessage(channel="C061QN2V6UA",text="Hello from python app")
+
+if __name__== "__main__":
+    app.run(debug=True)
